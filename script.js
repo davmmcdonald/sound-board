e323cfd07c3797c0322a0505d7e590141d39f6f7
David McDonald <26583259+davmmcdonald@users.noreply.github.com>
2022-11-22 12:45:13 -0600

    Made buttons clickable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function playSound(e) {
-    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const btn = document.querySelector(`.item[data-key="${e.keyCode}"]`);
+    const sound = document.querySelector(`audio[data-key="${e}"]`);
+    const btn = document.querySelector(`.item[data-key="${e}"]`);
     btn.classList.add("pressed");
     sound.currentTime = 0;
     sound.play();
@@ -11,7 +11,14 @@ function removeTransition(e) {
     this.classList.remove("pressed");
 }
 
-window.addEventListener("keydown",playSound);
+window.addEventListener("keydown",function(e) {
+    console.log(e);
+    playSound(e.keyCode);
+});
 
 const items = document.querySelectorAll(".item");
 items.forEach(item => item.addEventListener("transitionend",removeTransition));
+items.forEach(item => item.addEventListener('click',function(e) {
+    console.log(e);
+    playSound(e.target.getAttribute('data-key'));
+}));
